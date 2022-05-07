bccc564f2818736a1158b91bd5cfbfbcf321e7e4
Sahri Riza Umami <rizaumami@tutanota.com>
2022-05-07 14:19:52 +0700

    Sid support and restuctured script.

--- README.md
+++ README.md
@@ -1,19 +1,16 @@
-# debian-buster_bluetooth_aptX-LDAC
-Script installing aptX and LDAC codecs for bluetooth devices from Debian 10 buster.
-(Despite the name) It finally also works for *bullseye* and *bookworm (='testing' atm.)*. (This uses the method introduced with the buster-backport-repos to generate apt-/dpkg-compatible packages)
+# Debian_bluetooth_aptX-LDAC
+Script to install aptX and LDAC codecs for Debian system.
 
 Choose backports turned on to install as deb-packages.
 
-
-
 What does it do?
 - Install the required sofware packages
-- Backup the original libraries as <libname>.bak in their original folder.
+- Backup the original libraries to `$HOME`.
 - Clone the required sources from the EHfive project site and their submodules from pulseaudio and google-android repos
 - Compile them 
 - Install them
-  - with backports turned on: as deb-packages named "libldac" and "pulseaudio-module-bluetooth" (uninstall possible)
-  - w/o backports: normal 'make install'
-  - for Debian deleases 'bullseye' and 'bookworm' the dpkg-package method is used, as packages needed are available in th repos by default
+  - with backports turned on: as deb-packages named `libldac` and `pulseaudio-module-bluetooth` (uninstall possible)
+  - w/o backports: normal `make install`
+  - for Debian releases 'bullseye', 'bookworm' or 'sid' the dpkg-package method is used, as packages needed are available in th repos by default.
   
 For more details see the comments in the script.
--- bluetooth-aptx-ldac.sh
+++ bluetooth-aptx-ldac.sh
@@ -1,161 +1,181 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-# this script compiles the libraries and bluetooth modules to be able to use aptX and LDAC codecs via bluetooth
-# on debian buster (10)
+# Compiles the libraries and bluetooth modules to be able to use aptX and LDAC codecs via bluetooth on Debian.
 
 ####
-# 
-# Based on the great projects of EHfive (https://github.com/EHfive):
+#
+# Based on the great projects of EHfive:
 # https://github.com/EHfive/pulseaudio-modules-bt
 # https://github.com/EHfive/ldacBT
 #
 ####
 
-## add contrib and non-free repos as they are needed for some codecs (like aac) to install
+# VARIABLES --------------------------------------------------------------------
 
-sudo apt-add-repository contrib
-sudo apt-add-repository non-free
-sudo apt-get update
-# should be installed by default - just to be safe:
-sudo apt-get install -y lsb-release
+BTAPTXLDAC="$HOME/bt-aptX-LDAC"
+BAKDIR="$BTAPTXLDAC/backup/$(printf '%(%F_%T)T')"
 
+# FUNCTIONS --------------------------------------------------------------------
 
-# test on the cpu architecture used whether to install the fdkaac package
-if [[ "$(uname -m)"  == "x86_64" ]]
-then
-	fdkaac='fdkaac'
-else
-	fdkaac=''
-fi
+# Print message in brown.
+warning() {
+  printf '\e[33m:: %s\n\e[m' "$@"
+}
 
+hint() {
+  printf '\e[32m:: %s\n\e[m' "$@"
+}
 
-## ask user if the backports-repository should be activated to install this has to be done but only if the versin of the system is 'buster' and not already enabled
-if [[ "$(lsb_release -cs)" == "buster" ]] 
-then
-	if  ! apt-cache policy | grep -q buster-backports
-	then
-		read -p "Do you want to enable the backports repository on your system in order to use debian-packages installation ? y/n [n] " backports_enabled  
-    	
-	    	if [ "$backports_enabled" = "y" ]
-		then 
-			## add backports-repository to the source and reload apt cache to enable the necessary packages
-        		sudo apt-add-repository 'deb http://deb.debian.org/debian buster-backports main'
+print_usage() {
+  printf '%s\n' "
+  ${0##*/} is a script to compiles the libraries and bluetooth
+  modules to be able to use aptX and LDAC codecs via bluetooth on Debian.
 
-		    	# reload package-cache
-		    	sudo apt-get update
-		fi
-	else
-		backports_enabled="y"
-	fi
-	
-elif [[ "$(lsb_release -cs)" == "bullseye" ]]
-then
-        backports_enabled="y"
+  Usage: ${0##*/} OPTION
 
-elif [[ "$(lsb_release -cs)" == "bookworm" ]]
-then
-        backports_enabled="y"
+  OPTION:
+    -a            Automatically compiles libraries and modules.
+    -h  --help    Show this message and exit.
 
-else
-        echo -E "This is no Debian Buster or Bullseye or Bookworm (testing). Goodbye!"
-        exit 1
-fi
+  Example:
+    - ${0##*/} -a
+    - ${0##*/} -h
+"
+  exit
+}
 
-## installs the packages needed on normal debian buster (10) install
-if [ "$backports_enabled" = "y" ] 
-then 
-    sudo apt-get install git bluez-hcidump pkg-config cmake "$fdkaac" libtool libpulse-dev libdbus-1-dev libsbc-dev libbluetooth-dev libavcodec-dev libfdk-aac-dev git checkinstall
-else
-    sudo apt-get install git bluez-hcidump pkg-config cmake "$fdkaac" libtool libpulse-dev libdbus-1-dev libsbc-dev libavcodec-dev libbluetooth-dev libfdk-aac-dev git
-fi
+# MAIN -------------------------------------------------------------------------
 
-
-
-# backup original libraries
-echo -e "Backup:\n"
-MODDIR=`pkg-config --variable=modlibexecdir libpulse`
-sudo find $MODDIR -regex ".*\(bluez5\|bluetooth\).*\.so" -exec cp -v {} {}.bak \;
-
-
-## creates a temporary directory which is used for the compilation process 
-temp_compile_dir=$(mktemp -d)
-
-
-# jump into that directory
-cd "$temp_compile_dir"
-## compile libldac
-# check out the source from github
-git clone --recurse-submodules https://github.com/EHfive/ldacBT.git
-# jump into the dir
-cd ldacBT/
-# create a direcrtory
-mkdir build
-# jump in
-cd build
-# use the c-compiler with the given options
-cmake -DCMAKE_INSTALL_PREFIX=/usr -DINSTALL_LIBDIR=/usr/lib -DLDAC_SOFT_FLOAT=OFF ../
-# install the compiled thing
-
-if [ "$backports_enabled" = "y" ]
-then
-    echo -E ""
-    echo -E "####"
-    echo -E ""
-    echo -E "For ease of use you can just agree to all asked questions here with <enter>" 
-    sleep 4
-    sudo checkinstall -D --install=yes --pkgname libldac 
-else
-    sudo make DESTDIR=$DEST_DIR install
-fi
-
-
-## compile pulseaudio-modules-bt - same as above
-cd "$temp_compile_dir"
-
-git clone --recurse-submodules https://github.com/EHfive/pulseaudio-modules-bt.git
-cd pulseaudio-modules-bt
-git -C pa/ checkout v`pkg-config libpulse --modversion|sed 's/[^0-9.]*\([0-9.]*\).*/\1/'`
-mkdir build
-cd build
-cmake ..
-make
-
-if [ "$backports_enabled" = "y" ]
-then
-    echo -E ""
-    echo -E "####"
-    echo -E ""
-    echo -E "For ease of use you can just agree to all asked questions here with <enter>" 
-    sleep 4 
-    sudo checkinstall -D --install=yes --pkgname pulseaudio-module-bluetooth
-else
-    sudo make install
-fi
-
-
-
-## configure pulseaudio to use LDAC in high quality - ask user if this has to be done
-read -p "Do you want to force using LDAC-codec in high quality? y/n [n] " answer
-if [ "$answer" = "y" ]
-then 
-    # exchange text in the pulseaudio config - in front make a copy name <filename.bak> in same folder
-    sudo sed -i.bak 's/^load-module module-bluetooth-discover$/load-module module-bluetooth-discover a2dp_config="ldac_eqmid=hq ldac_fmt=f32"/g' /etc/pulse/default.pa
-fi
- 
-
-# restart pulseaudio and bluetooth service
-pulseaudio -k
-sudo systemctl restart bluetooth.service
-
-
-# User messages and infos
-echo ''
-echo '#################################'
-echo '#################################'
-echo -E "To test which codec is used for your device, disconnect your device, start this command: sudo hcidump | grep -A 10 -B 10 'Set config', then reconnect your device."
-echo -E "Check the line with 'Media Codec - non-A2DP (xyz)' below 'Set config'"
-echo -E "To configure the codec manually check the options for /etc/pulse/default.pa here: https://github.com/EHfive/pulseaudio-modules-bt#configure"
-
-
-sudo rm -R "$temp_compile_dir"
-
+case "$1" in
+ -a)
+    mkdir -p "$BAKDIR"
+    cd "$BTAPTXLDAC" || exit
+    
+    # Add contrib and non-free repos as they are needed for some codecs (like AAC) to install.
+    sudo apt-add-repository contrib
+    sudo apt-add-repository non-free
+    sudo apt-get update
+    # Should be installed by default - just to be safe:
+    sudo apt-get install -y lsb-release
+    
+    # Test on the CPU architecture used whether to install the fdkaac package.
+    if [[ "$(uname -m)" == 'x86_64' ]]; then
+      fdkaac='fdkaac'
+    else
+      fdkaac=''
+    fi
+    
+    # Ask user if the backports-repository should be activated to install this has to be done but only if the version of the system is 'buster' and not already enabled.
+    if [[ "$(lsb_release -cs)" == 'buster' ]]; then
+      if ! apt-cache policy | grep -q buster-backports; then
+        read -r -p 'Do you want to enable the backports repository on your system in order to use debian-packages installation ? [y/N]' backports_enabled
+    
+        if [[ "$backports_enabled" == 'y' ]]; then
+          # Add backports-repository to the source and reload apt cache to enable the necessary packages.
+          sudo apt-add-repository 'deb http://deb.debian.org/debian buster-backports main'
+    
+          # Reload package-cache.
+          sudo apt-get update
+        fi
+      else
+        backports_enabled='y'
+      fi
+    elif [[ "$(lsb_release -cs)" == 'bullseye' ]]; then
+      backports_enabled='y'
+    elif [[ "$(lsb_release -cs)" == 'bookworm' ]]; then
+      backports_enabled='y'
+    elif [[ "$(lsb_release -cs)" == 'sid' ]]; then
+      backports_enabled='n'
+    else
+      warning 'This Debian release is not suported.' 
+      exit 1
+    fi
+    
+    # Installs the packages needed.
+    if [[ "$backports_enabled" == 'y' ]]; then
+      sudo apt-get install git bluez-hcidump pkg-config cmake "$fdkaac" libtool libpulse-dev libdbus-1-dev libsbc-dev libbluetooth-dev libavcodec-dev libfdk-aac-dev libltdl-dev git checkinstall
+    else
+      sudo apt-get install git bluez-hcidump pkg-config cmake "$fdkaac" libtool libpulse-dev libdbus-1-dev libsbc-dev libavcodec-dev libbluetooth-dev libfdk-aac-dev libltdl-dev git
+    fi
+    
+    # Backup original libraries.
+    MODDIR=$(pkg-config --variable=modlibexecdir libpulse)
+    hint "Backing up $MODDIR" 
+    mkdir -p "$BAKDIR/$MODDIR"
+    sudo find "$MODDIR" -regex ".*\(bluez5\|bluetooth\).*\.so" -exec cp {} "$BAKDIR/$MODDIR" \;
+    
+    # Compile libldac.
+    # Check out the source from github.
+    git clone --recurse-submodules https://github.com/EHfive/ldacBT.git
+    # Jump into the dir.
+    cd ldacBT/ || exit
+    # Create a directory.
+    mkdir -p build
+    # Jump in.
+    cd build || exit
+    # Use the c-compiler with the given options.
+    cmake -DCMAKE_INSTALL_PREFIX=/usr -DINSTALL_LIBDIR=/usr/lib -DLDAC_SOFT_FLOAT=OFF ../
+    # Install the compiled thing.
+    
+    if [[ "$backports_enabled" == 'y' ]]; then
+      hint 'You can just agree to all asked questions here with <enter>'
+      sleep 4
+      sudo checkinstall -D --install=yes --pkgname libldac
+    else
+      sudo make DESTDIR="$DEST_DIR" install
+    fi
+    
+    # Compile pulseaudio-modules-bt - same as above.
+    cd "$BTAPTXLDAC" || exit
+    
+    git clone --recurse-submodules https://github.com/EHfive/pulseaudio-modules-bt.git
+    cd pulseaudio-modules-bt || exit
+    git -C pa/ checkout v"$(pkg-config libpulse --modversion | sed 's/[^0-9.]*\([0-9.]*\).*/\1/')"
+    mkdir -p build
+    cd build || exit
+    cmake ..
+    make
+    
+    if [[ "$backports_enabled" == 'y' ]]; then
+      hint 'You can just agree to all asked questions here with <enter>'
+      sleep 4
+      sudo checkinstall -D --install=yes --pkgname pulseaudio-module-bluetooth
+    else
+      sudo make install
+    fi
+    
+    # Configure pulseaudio to use LDAC in high quality - ask user if this has to be done.
+    read -r -p '
+    Do you want to force using LDAC-codec in high quality? [y/N]' answer
+    if [[ "$answer" == 'y' ]]; then
+      # Exchange text in the pulseaudio config - in front make a copy name <filename.bak> in same folder.
+      cp -v /etc/pulse/default.pa "$BAKDIR"
+      sudo sed -i 's/^load-module module-bluetooth-discover$/load-module module-bluetooth-discover a2dp_config="ldac_eqmid=hq ldac_fmt=f32"/g' /etc/pulse/default.pa
+    fi
+    
+    # Restart pulseaudio and bluetooth service.
+    pulseaudio -k
+    sudo systemctl restart bluetooth.service
+    
+    # If error: br-connection-profile-unavailable.
+    pacmd load-module module-bluetooth-policy
+    pacmd load-module module-bluetooth-discover
+    
+    # User messages and infos.
+    printf '%s\n' "
+  To test which codec is used for your device:
+  1. disconnect your device
+  2. start this command: 
+     sudo hcidump | grep -A 10 -B 10 'Set config'
+  3. reconnect your device.
+  
+  Check the line with 'Media Codec - non-A2DP (xyz)' below 'Set config'
+  
+  To configure the codec manually check the options for /etc/pulse/default.pa
+  here: https://github.com/EHfive/pulseaudio-modules-bt#configure'
+    "
+  ;;
+  *)
+    print_usage
+  ;;
+esac 
